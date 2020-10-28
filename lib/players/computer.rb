de412aa4b0e2fa7906a5de82097857c62794d1ea
John Stassen <john.stassen@gmail.com>
2020-10-28 02:43:59 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -6,8 +6,8 @@ module Players
   WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
   def move(board)
-    win
-    prevent_loss
+    win(board)
+    prevent_loss(board)
     maximize_potential_victories
   end
 
